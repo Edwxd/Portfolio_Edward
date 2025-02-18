9964d091d3fd6356153bbebe28841bb71594b326
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-02-18 02:15:31 -0500

    modified all applicationEndPoints

--- Portfolio-BE/src/main/java/com/example/portfoliobe/Projectssubdomain/presentationlayer/ProjectsController.java
+++ Portfolio-BE/src/main/java/com/example/portfoliobe/Projectssubdomain/presentationlayer/ProjectsController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/v1/projects")
-@CrossOrigin(origins = "https://portfolioedward-production.up.railway.app")
+//@CrossOrigin(origins = "https://portfolioedward-production.up.railway.app")
 
 //@CrossOrigin(origins = "http://localhost:3000")
 public class ProjectsController {
