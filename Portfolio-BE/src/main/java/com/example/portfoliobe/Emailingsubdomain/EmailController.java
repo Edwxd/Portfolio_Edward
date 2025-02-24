58377a5d103c7ba23027b4c9efdb99d655cfe4a8
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-02-23 21:57:17 -0500

    added backend logic

---
+++ Portfolio-BE/src/main/java/com/example/portfoliobe/Emailingsubdomain/EmailController.java
@@ -0,0 +1,23 @@
+package com.example.portfoliobe.Emailingsubdomain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class EmailController {
+    @Autowired
+    private EmailSenderService senderService;
+
+    @GetMapping("/api/v1/send-email")
+    public Mono<String> sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        return senderService.sendEmail(to, subject, body)
+                .then(Mono.just("Email sent successfully"))
+                .onErrorResume(e -> {
+                    e.printStackTrace();
+                    return Mono.just("Failed to send email: " + e.getMessage());
+                });
+    }
+
+}
---
+++ Portfolio-BE/src/main/java/com/example/portfoliobe/Emailingsubdomain/EmailSenderService.java
@@ -0,0 +1,32 @@
+package com.example.portfoliobe.Emailingsubdomain;
+
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class EmailSenderService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public Mono<Void> sendEmail(String to, String subject, String body) {
+        return Mono.fromRunnable(() -> {
+            try {
+
+                MimeMessage message = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+                helper.setTo(to);
+                helper.setSubject(subject);
+                helper.setText(body, true);
+
+                mailSender.send(message);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to send email to " + to, e);
+            }
+        }).then();
+    }
+}
