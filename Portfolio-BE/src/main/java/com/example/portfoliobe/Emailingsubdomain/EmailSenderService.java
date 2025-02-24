ac74b21d23d9ab9019c9ca82be78a86a3ed2a4e6
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-02-23 22:00:52 -0500

    fixed backend

--- Portfolio-BE/src/main/java/com/example/portfoliobe/Emailingsubdomain/EmailSenderService.java
+++ Portfolio-BE/src/main/java/com/example/portfoliobe/Emailingsubdomain/EmailSenderService.java
@@ -1,6 +1,9 @@
 package com.example.portfoliobe.Emailingsubdomain;
 
 
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
