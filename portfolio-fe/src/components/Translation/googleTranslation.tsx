73fc220ebc9ad3e7a87007baa13a1181b143c48c
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-02-24 23:38:00 -0500

    fixed bug

--- portfolio-fe/src/components/Translation/googleTranslation.tsx
+++ portfolio-fe/src/components/Translation/googleTranslation.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "../App.css"
+
 
 export default function GoogleTranslateLoader(): null {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
