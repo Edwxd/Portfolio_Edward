f92c0ac7a8c7c217746664fe3d80ea3a0a32a310
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-05-20 17:31:52 -0400

    setting changes

--- Portfolio-BE/src/main/java/com/example/portfoliobe/PortfolioBeApplication.java
+++ Portfolio-BE/src/main/java/com/example/portfoliobe/PortfolioBeApplication.java
@@ -2,13 +2,17 @@ package com.example.portfoliobe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        MetricsAutoConfiguration.class,
+        SystemMetricsAutoConfiguration.class,
+        SimpleMetricsExportAutoConfiguration.class
+})
 public class PortfolioBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PortfolioBeApplication.class, args);
     }
-
 }
